dbf9cc6e943efc276e3f02827366e1031ea00dd5
zsan <zaksantanu@gmail.com>
2021-08-21 04:54:56 +0700

    remove helloword

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <page-home/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageHome from '@/components/PageHome'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PageHome
   }
 }
 </script>
