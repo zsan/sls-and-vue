45568591472471a5e2e8c9efc14fd3ea19219c07
zsan <zaksantanu@gmail.com>
2021-08-24 22:04:59 +0700

    beforeEnter (redirect thing)

--- src/components/PageNotFound.vue
+++ src/components/PageNotFound.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="coll-full">
+  <div class="coll-full text-center">
     <h1>Not Found</h1>
     <router-link :to="{ name: 'Home'}">Read some cool threads</router-link>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import PageHome from '@/components/PageHome'
 import PageThreadShow from '@/components/PageThreadShow'
 import PageNotFound from '@/components/PageNotFound'
+import sourceData from '@/data.json'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
@@ -13,7 +14,25 @@ const routes = [
     path: '/thread/:id',
     component: PageThreadShow,
     name: 'ThreadShow',
-    props: true
+    props: true,
+    beforeEnter (to, from, next) {
+      // check if thread exists
+      const threadExists = sourceData.threads.find(thread => thread.id == to.params.id)
+      if (threadExists) {
+        return next()
+
+      // if doesnt exists redirect to not found
+      } else {
+        next({
+          name: "NotFound",
+          params: {
+            pathMatch: to.path.substring(1).split('/')
+          },
+          query: to.query,
+          hash: to.hash
+        })
+      }
+    }
   },
   {
     path: '/:pathMatch(.*)*',
