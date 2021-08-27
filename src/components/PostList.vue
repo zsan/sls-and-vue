53aee04308d4036ab4700edb1d87a0bc4ee481b3
zsan <zaksantanu@gmail.com>
2021-08-27 23:35:15 +0700

    humanize date

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -17,8 +17,8 @@
         </div>
       </div>
 
-      <div class="post-date text-faded">
-        {{ post.publishedAt }}
+      <div class="post-date text-faded" :title="humanFriendlyDate(post.publishedAt)">
+        {{ diffForHumans(post.publishedAt) }}
       </div>
     </div>
   </div>
@@ -26,6 +26,13 @@
 
 <script>
 import sourceData from '@/data.json'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import localizedDate from 'dayjs/plugin/localizedFormat'
+
+dayjs.extend(relativeTime)
+dayjs.extend(localizedDate)
+
 export default {
   props: {
     posts: {
@@ -41,6 +48,12 @@ export default {
   methods: {    
     userById (userId) {
       return this.users.find(p => p.id === userId)
+    },
+    diffForHumans(timestamp) {
+      return dayjs.unix(timestamp).fromNow()
+    },
+    humanFriendlyDate(timestamp) {
+      return dayjs.unix(timestamp).format('llll')
     }
   }
 }
