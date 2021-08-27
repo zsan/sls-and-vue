44f254b7c0110f5832b4e009e4b39c0c7dbe3c17
zsan <zaksantanu@gmail.com>
2021-08-27 20:56:15 +0700

    post form

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -3,6 +3,18 @@
     <h1>{{ thread.title }}</h1>
 
     <post-list :posts="threadPosts"/>
+
+    <div class="col-full">
+
+      <form @submit.prevent="addPost">
+        <div class="form-group">
+          <textarea v-model="newPostText" name="" id="" rows="10" class="form-input" />
+        </div>
+        <div class="form-actions">
+          <button class="btn-blue">Submit post</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -24,7 +36,8 @@ export default {
   data(){
     return {
       threads: sourceData.threads,
-      posts: sourceData.posts
+      posts: sourceData.posts,
+      newPostText: ''
     }
   },
   computed: {
@@ -33,6 +46,24 @@ export default {
     },
     threadPosts(){
       return this.posts.filter(post => post.threadId == this.id)
+    }
+  },
+  methods: {
+    addPost() {
+      const postId = 'qqqq' + Math.random()
+      const post = {
+        id: postId,
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: "ALXhxjwgY9PinwNGHpfai6OWyDu2"
+      }
+
+      this.posts.push(post)
+      this.thread.posts.push(postId)
+
+      this.newPostText = ''
+
     }
   }
 }
